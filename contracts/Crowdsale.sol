8fea75492fdf1ed8d9ecb833b40e84a7dd02aa5b
SonderVision <sondervisionico@gmail.com>
2018-03-17 17:00:58 +0200

    add phase bonuses

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -9,6 +9,49 @@ contract SonderICO is Crowdsale {
   uint256 public bonus2Rate = publicRate * 110 / 100;
   uint256 public bonus3Rate = publicRate * 105 / 100;
 
-  function SonderICO(address _wallet, ERC20 _token) Crowdsale(bonus1Rate, _wallet, _token) {
+  uint256 public startDate;
+  uint256 public phase1EndDate;
+  uint256 public phase2EndDate;
+  uint256 public phase3EndDate;
+
+  function SonderICO(address _wallet, ERC20 _token, uint256 _startDate, uint256 _phase1EndDate, uint256 _phase2EndDate, uint256 _phase3EndDate)
+    Crowdsale(bonus1Rate, _wallet, _token) {
+      startDate = _startDate;
+      phase1EndDate = _phase1EndDate;
+      phase2EndDate = _phase2EndDate;
+      phase3EndDate = _phase3EndDate;
+  }
+
+  function isValidRate() internal view returns (bool) {
+    if (now >= phase3EndDate) return rate == publicRate;
+    if (now >= phase2EndDate) return rate <= bonus3Rate;
+    if (now >= phase1EndDate) return rate <= bonus2Rate;
+    return true;
+  }
+
+  function currentStage() public view returns (uint) {
+    if (now >= phase3EndDate) return 4;
+    if (now >= phase2EndDate) return 3;
+    if (now >= phase1EndDate) return 2;
+    if (now >= startDate) return 1;
+    return 0;
+  }
+
+  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+    super._preValidatePurchase(_beneficiary, _weiAmount);
+    require(now >= startDate);
+
+    if (!isValidRate()) {
+      uint stage = currentStage();
+      if (stage == 4) {
+        rate = publicRate;
+      } else if (stage == 3) {
+        rate = bonus3Rate;
+      } else if (stage == 2) {
+        rate = bonus2Rate;
+      } else if (stage == 1) {
+        rate = bonus1Rate;
+      }
+    }
   }
 }
--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -1,8 +1,16 @@
 const SonderToken = artifacts.require("./SonderToken.sol");
 const SonderICO = artifacts.require("./SonderICO.sol");
 
+const wallet = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
+const icoTokenAmount = 240e6;
+const startDate = Date.parse('03/25/2018') / 1000; // 03/25
+const phase1EndDate = Date.parse('03/28/2018') / 1000; // 03/28
+const phase2EndDate = Date.parse('04/05/2018') / 1000; // 04/05
+const phase3EndDate = Date.parse('04/11/2018') / 1000; // 04/11
+
 module.exports = async function(deployer) {
-  const testWallet = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
   await deployer.deploy(SonderToken);
-  deployer.deploy(SonderICO, testWallet, SonderToken.address);
+  const token = await SonderToken.deployed();
+  await deployer.deploy(SonderICO, wallet, SonderToken.address, startDate, phase1EndDate, phase2EndDate, phase3EndDate);
+  token.mint(SonderICO.address, web3.toWei(icoTokenAmount, 'ether'));
 };
