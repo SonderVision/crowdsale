8f3cc4ff13ff25e8001c7c9b446fd5c805fe179d
SonderVision <sondervisionico@gmail.com>
2018-03-25 15:26:09 +0300

    leave only a crowdsle contract part

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -5,24 +5,15 @@ import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 
 contract SonderICO is Crowdsale, Ownable {
-  uint256 public publicRate = 14286;
-  uint256[] rates = [publicRate * 115 / 100, publicRate * 110 / 100, publicRate * 105 / 100, publicRate];
-  uint[] minDeposit = [10000, 5000, 1000, 100];
-  uint stage = 0;
+  uint minDeposit = 100;
 
   uint256 public startDate;
-  uint256 public phase1EndDate;
-  uint256 public phase2EndDate;
-  uint256 public phase3EndDate;
 
   bool public isActive = true;
 
-  function SonderICO(address _wallet, ERC20 _token, uint256 _startDate, uint256 _phase1EndDate, uint256 _phase2EndDate, uint256 _phase3EndDate)
-    Crowdsale(rates[stage], _wallet, _token) {
+  function SonderICO(address _wallet, ERC20 _token, uint256 _startDate)
+    Crowdsale(14286, _wallet, _token) {
       startDate = _startDate;
-      phase1EndDate = _phase1EndDate;
-      phase2EndDate = _phase2EndDate;
-      phase3EndDate = _phase3EndDate;
   }
 
   modifier onlyWhileActive {
@@ -36,30 +27,8 @@ contract SonderICO is Crowdsale, Ownable {
     token.transfer(owner, token.balanceOf(this));
   }
 
-  function getStageByTime() internal view returns (uint) {
-    if (now >= phase3EndDate) return 3;
-    if (now >= phase2EndDate) return 2;
-    if (now >= phase1EndDate) return 1;
-    return 0;
-  }
-
-  function setStage() internal {
-    uint currentStage = getStageByTime();
-    if (currentStage > stage) {
-      stage = currentStage;
-      rate = rates[stage];
-    }
-  }
-
-  //fallback function in case we'll need to close bonus stage phase earlier
-  function manuallySetStage(uint _stage) external onlyOwner {
-    stage = _stage;
-    rate = rates[stage];
-  }
-
   function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal onlyWhileActive {
     super._preValidatePurchase(_beneficiary, _weiAmount);
-    setStage();
-    require(_weiAmount >= minDeposit[stage] * (10 ** 15));
+    require(_weiAmount >= minDeposit * (10 ** 15));
   }
 }
--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -3,14 +3,11 @@ const SonderICO = artifacts.require("./SonderICO.sol");
 
 const wallet = '0xf17f52151EbEF6C7334FAD080c5704D77216b732';
 const icoTokenAmount = 240e6;
-const startDate = Date.parse('03/23/2018') / 1000; // 03/25
-const phase1EndDate = Date.parse('03/28/2018') / 1000; // 03/28
-const phase2EndDate = Date.parse('04/05/2018') / 1000; // 04/05
-const phase3EndDate = Date.parse('04/11/2018') / 1000; // 04/11
+const startDate = Date.parse('04/11/2018') / 1000; // 04/11
 
 module.exports = async function(deployer) {
   await deployer.deploy(SonderToken);
   const token = await SonderToken.deployed();
-  await deployer.deploy(SonderICO, wallet, SonderToken.address, startDate, phase1EndDate, phase2EndDate, phase3EndDate);
+  await deployer.deploy(SonderICO, wallet, SonderToken.address, startDate);
   token.mint(SonderICO.address, web3.toWei(icoTokenAmount, 'ether'));
 };
